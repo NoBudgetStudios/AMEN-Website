58542ba7e484b230a845c3f21c1df085a3c8fe79
kliklis <kklimantakis@gmail.com>
2024-10-01 11:18:37 +0300

    committed at Tue-10-01_2024-11

--- googleAnalytics.js
+++ googleAnalytics.js
@@ -1,4 +1,15 @@
+// Load the Google Tag Manager script
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-SYVPP6RPJC';
+  document.head.appendChild(script);
+})();
+
+// Initialize Google Analytics
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
+
+// Configure the tag
 gtag('js', new Date());
 gtag('config', 'G-SYVPP6RPJC');
