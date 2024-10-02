e2cdad9ad1d79d2eb1ae5c419e353847246873f0
kliklis <kklimantakis@gmail.com>
2024-10-02 16:01:16 +0300

    committed at Wed-10-02_2024-16

--- googleAnalytics.js
+++ googleAnalytics.js
@@ -1,10 +1,10 @@
 // Load the Google Tag Manager script
-/*(function() {
+(function() {
   var script = document.createElement('script');
   script.async = true;
   script.src = 'https://www.googletagmanager.com/gtag/js?id=G-SYVPP6RPJC';
   document.head.appendChild(script);
-})();*/
+})();
 
 // Initialize Google Analytics
 window.dataLayer = window.dataLayer || [];
@@ -13,3 +13,22 @@ function gtag(){dataLayer.push(arguments);}
 // Configure the tag
 gtag('js', new Date());
 gtag('config', 'G-SYVPP6RPJC');
+
+
+////button tracking
+
+// Add event listener for the button click event
+document.getElementById('newsletter-button').addEventListener('click', function() {
+  gtag('event', 'click', {
+    'event_category': 'Button',
+    'event_label': 'Join Newsletter'
+  });
+});
+
+// Add event listener for the button click event
+document.getElementById('rules-download-button').addEventListener('click', function() {
+  gtag('event', 'click', {
+    'event_category': 'Button',
+    'event_label': 'Download Rulebook'
+  });
+});
