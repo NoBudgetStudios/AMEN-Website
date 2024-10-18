22c1106edf88749ee9c4f12eac834e7650a4a3d6
kliklis <kklimantakis@gmail.com>
2024-10-18 15:45:03 +0300

    committed at Fri-10-18_2024-15

--- dynamicContentLoader.js
+++ dynamicContentLoader.js
@@ -58,7 +58,7 @@ const promoImage8 = "./images/promo-material/promo_image (8).png";
 const promoImage9 = "./images/promo-material/promo_image (9).png";
 
 //URLS
-const pdfDownloadLink = "https://drive.google.com/uc?export=download&id=1kYb3szWHzU7T_dN2otoGw46lpggPG5mP";
+const pdfDownloadLink = "https://drive.google.com/uc?export=download&id=1uRRUbz8ru8EnD7du42cqe_rcPhQBCGmH";
 
 
 function updatePageContent() {
